d01589670b5b73036515adbe22c29e216711df6a
Juan Pablo <jvelezor@unal.edu.co>
2023-12-16 15:06:25 -0500

    Corrección

--- Modelo_loteal2.m
+++ Modelo_loteal2.m
@@ -1,32 +1,29 @@
-function vard=Modelo_loteal2(var,u1,u2,u3,u4)
+function dYdt=Modelo_loteal2(var,u1,u2,u3,u4,u5)
+   
+    % Parameters
+    mu_max = u1;      % 1/h
+    Y_XS = u2;        % g/g
+    Ks = 0.08;          % g/L
+    S_in = 20;         % g/L (substrate concentration in feed)
+    Smax = 30;         %g/L
+    ms = 0.00001;
+    alfa = u3;
+    beta = u4;
+    F=u5;
 
-%Llamar parámetros
-umax = u1;%0.83;
-Yxs= u2;%0.8;
-Ks=0.08;
-alfa=0.05;
-beta=0.002;
-Smax=30;
-ms=0.00001;
-Sin=u3;
-F=u4;
-
-
-%Definir dinámicas
-X=var(1);
-S=var(2);
-P=var(3);
-V=var(4);
-
-%Ecuaciones constitutivas
-u=umax*(S/(Ks+S))*(1-(S/Smax));
-Rx =-u*X;
-Rp=(alfa*u+beta)*X;
-Rs=((u/Yxs)+ms)*X;
-
-%Ecuaciones diferenciales
-vard(1)=Rx-(F/V)*X;
-vard(2)=(F/V)*(Sin-S)-Rs;
-vard(3)=Rp-(F/V)*P;
-vard(4)=F;
-vard=vard';
+    % Current states
+    X = var(1);          % Biomass concentration (g/L)
+    S = var(2);          % Substrate concentration (g/L)
+    P = var(3);          % Product concentration (g/L)
+    V = var(4);          % Volume (L)
+    
+    % Monod Growth kinetics
+    mu = mu_max * S / (Ks + S)* (1-(S/(Smax)));
+    
+    % Mass balances
+    dXdt = mu * X - (F/V) * X;
+    dSdt = (F/V) * (S_in - S) - ((mu / Y_XS) + ms) * X;
+    dPdt = (alfa * mu + beta) * X - (F/V) * P;
+    dVdt = F;
+    
+    dYdt = [dXdt, dSdt, dPdt, dVdt];
